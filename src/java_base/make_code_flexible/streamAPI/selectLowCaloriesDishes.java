5306b5801eaed445f28ddd3221aa675aee7b6383
sxtykyj <yujie.kang0925@hotmail.com>
2019-10-05 23:04:12 -0400

    《Java8实战》第5章 流的常用方法介绍

--- src/java_base/make_code_flexible/streamAPI/selectLowCaloriesDishes.java
+++ src/java_base/make_code_flexible/streamAPI/selectLowCaloriesDishes.java
@@ -43,8 +43,41 @@ public class selectLowCaloriesDishes {
                         .map(Dish::getName)
                         //截断流，只选calories最低的三个dishes，返回流
                         .limit(3)
+                        // 根据流所生成元素的hashcode和equals方法实现，去掉重复数据
+                        .distinct()
                         // 处理流水线，赋map值给lowCaloricDishesName，并转换成list返回结果，关闭流
                         .collect(toList());
+        /**
+         * Array.stream(arr)方法可以接受一个数组并产生一个流
+         * 其他流的常用方法：
+         *    .skip（n）：跳过前n个符合条件的数据
+         *    .map(Array::stream): 让每个数组变成一个单独的流
+         *    .flatMap(Array::stream): 让每个数组映射成流的内容，即会合并上一个方法生成的所有单个流为一个流
+         *    .allMatch(condition): 流中是否有一个元素能匹配，返回boolean
+         *    .anyMatch(condition): 是否有匹配流中所有元素，返回boolean
+         *    .noneMatch(condition): 是否流中所有元素都不匹配，返回boolean
+         *    .findFirst() /  .findAny()
+         *
+         * 对流中所有元素求和，sum初始值为0
+         *    int sum = numbers.stream().reduce(0, (a,b) -> a + b)；
+         *    简化版： int sum = numbers.stream().reduce(0, Integer::sum)；
+         *            //sum, max, min   <int max = maxCalories.orElse(1); 加默认值>
+         *
+         * 优化装箱造成的复杂性：
+         *    .map(Dish::getCalories).reduce(0, Integer::sum)；  =  .mapToInt(Dish::getCalories).sum()；
+         *
+         * 转换回对象流：
+         *     IntStream intStream = menu.stream().mapToInt(Dish::getCalories);    //将stream转换成数值流
+         *     Stream<Integer> stream = intStream.boxed();                         //将数值转回stream
+         *
+         * 数值范围：
+         *      .range(1, 100)           不包括100
+         *      .rangeClosed(1, 100)     包括100
+         *     IntStream evenNumbers = IntStream.rangeClosed(1, 100).filter(n -> n % 2 == 0)；
+         *     System.out.println(evenNumbers.count());
+         *
+         *
+         */
         System.out.println(lowCaloricDishesName);
     }
 }
